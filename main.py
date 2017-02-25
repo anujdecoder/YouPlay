30f260178a2d136dcdd51727857e628ec97e5ca4
Anuj Gupta <anupta45@gmail.com>
2017-02-25 21:59:44 +0530

    Main fetches the number of videos and retrieves all the links

--- main.py
+++ main.py
@@ -2,30 +2,44 @@ import pafy
 import urllib.request
 
 print("Enter the URL of playlist");
-playlist=input();
+playlist = input();
 
-print("Enter the total number of videos");
-num=int(input());
+print("The total number of videos are", end=" ");
 
-response=urllib.request.urlopen(playlist)
-str=str(response.read())
+response = urllib.request.urlopen(playlist)
+str = str(response.read())
+
+p = str.index("pl-header-details")
+number_of_videos = str[p:]
+
+p = 0
+p = number_of_videos.index("</li><li>")
+number_of_videos = number_of_videos[p:]
+
+p = 0
+p = number_of_videos.index("videos") - 1
+
+number_of_videos = number_of_videos[0:p]
+number_of_videos = number_of_videos[9:]
+number_of_videos = int(number_of_videos)
+num = number_of_videos
+print(num)
 
 for i in range(num):
-    a=str.index("pl-video-title-link yt-uix-tile-link yt-uix-sessionlink")
-    str=str[a:]
-    a=0;
-    b=str.index("pl-video-bottom-standalone-badge")
-    link=str[a:b]
+    a = str.index("pl-video-title-link yt-uix-tile-link yt-uix-sessionlink")
+    str = str[a:]
+    a = 0;
+    b = str.index("pl-video-bottom-standalone-badge")
+    link = str[a:b]
 
-    x=link.index('href')
-    y=link.index("data-sessionlink")-2
-    x=x+6
-    link=link[x:y]
+    x = link.index('href')
+    y = link.index("data-sessionlink") - 2
+    x = x + 6
+    link = link[x:y]
 
-    
-    str=str[b:]
-    
-    print("https://www.youtube.com"+link+"\n")
+    str = str[b:]
+
+    print("https://www.youtube.com" + link + "\n")
 
 
-    
+
